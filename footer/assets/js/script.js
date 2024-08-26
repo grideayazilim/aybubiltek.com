b79d886e14e026c0179aaf3f8a9002051a68a3a5
erto7 <ebulgen7@gmail.com>
2024-08-27 01:45:28 +0300

    footer without responsive design

--- footer/assets/js/script.js
+++ footer/assets/js/script.js
@@ -1,1 +1,13 @@
-/*============== TOPIC NAME ==============*/
+/*============== FOOTER ==============*/
+
+//"Biltek'li Ol" button.
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  button.classList.add("animate");
+
+  setTimeout(() => {
+    button.classList.remove("animate");
+  }, 400);
+});
