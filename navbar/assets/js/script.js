6b742804577eac92f5e032b34f907c794171afdf
Enes Eldeş <ns.eldes51@gmail.com>
2024-08-27 17:09:11 +0300

    Added navbar structure
    Code enhancement and minor changes will be made

--- navbar/assets/js/script.js
+++ navbar/assets/js/script.js
@@ -1,1 +1,27 @@
-/*============== TOPIC NAME ==============*/
+/*============== ben de bilmiyom ==============*/
+const activatorItem = document.querySelector(".activator-item");
+const openSubmenu = activatorItem.querySelector(".sublist");
+
+// Use closures to pass `openSubmenu` to the event handlers
+activatorItem.addEventListener("mouseover", () => {
+  openSubmenu.style.height = openSubmenu.scrollHeight + "px";
+});
+
+activatorItem.addEventListener("mouseout", () => {
+  openSubmenu.removeAttribute("style");
+});
+
+/*============== TOGGLE MENU ==============*/
+const closeButton = document.querySelector(".nav-close");
+const toggleButton = document.querySelector(".nav-toggle");
+const toggleMenu = document.querySelector(".nav-menu");
+
+// Show menu
+toggleButton.addEventListener("click", () => {
+  toggleMenu.classList.add("show-menu");
+});
+
+// Close menu
+closeButton.addEventListener("click", () => {
+  toggleMenu.classList.remove("show-menu");
+});
