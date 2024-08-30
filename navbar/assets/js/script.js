986b9ea4a8b5896816e329ef21d198711a4f5cdb
Enes Eldeş <ns.eldes51@gmail.com>
2024-08-30 21:51:33 +0300

    Added js scripts of blurring and whitening navbar

--- navbar/assets/js/script.js
+++ navbar/assets/js/script.js
@@ -1,14 +1,16 @@
-/*============== ben de bilmiyom ==============*/
-const activatorItem = document.querySelector(".activator-item");
-const openSubmenu = activatorItem.querySelector(".sublist");
+/*============== değiştir burayı ==============*/
+const activatorItem = document.querySelectorAll(".activator-item");
 
-// Use closures to pass `openSubmenu` to the event handlers
-activatorItem.addEventListener("mouseover", () => {
-  openSubmenu.style.height = openSubmenu.scrollHeight + "px";
-});
+activatorItem.forEach((item) => {
+  const openSubmenu = item.querySelector(".sublist");
 
-activatorItem.addEventListener("mouseout", () => {
-  openSubmenu.removeAttribute("style");
+  item.addEventListener("mouseover", () => {
+    openSubmenu.style.height = openSubmenu.scrollHeight + "px";
+  });
+
+  item.addEventListener("mouseout", () => {
+    openSubmenu.removeAttribute("style");
+  });
 });
 
 /*============== TOGGLE MENU ==============*/
@@ -25,3 +27,40 @@ toggleButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   toggleMenu.classList.remove("show-menu");
 });
+
+/*============== BLUR AND WHITEN NAVBAR ==============*/
+const navbar = document.querySelector("nav");
+const blurDiv = document.getElementById("blur-div");
+let isHomePage = "isHomePage";
+
+//Check if the site is home page
+document.getElementById("home-page") === null
+  ? (isHomePage = false)
+  : (isHomePage = true);
+
+/*
+  Navbar background color structure:
+
+  At the top of the page:
+    -->If user is in the homepage, transparent background.
+    -->If not, white background.
+  When scrolled:
+    -->Blurred and translucent background.
+
+  --------------------------------------------------------
+
+  handleNavbarBackground implements the structure above.
+*/
+const handleNavbarBackground = () => {
+  if (!isHomePage) navbar.classList.add("white-container");
+
+  if (this.scrollY >= 50) {
+    blurDiv.classList.remove("anti-blur");
+    if (!isHomePage) navbar.classList.remove("white-container");
+  } else {
+    blurDiv.classList.add("anti-blur");
+    if (!isHomePage) navbar.classList.add("white-container");
+  }
+};
+
+window.addEventListener("scroll", handleNavbarBackground);
