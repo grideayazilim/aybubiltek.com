5184efdf5a63cd8e5b9dcacae2af26d75699b00d
haticeakkus <hticeakks1@gmail.com>
2024-09-13 20:50:29 +0300

    Added landing page with responsive design

--- home-page/landing-page/assets/js/script.js
+++ home-page/landing-page/assets/js/script.js
@@ -1,1 +1,11 @@
-/*============== TOPIC NAME ==============*/
+    function changeImage() {
+        var bgImage = document.getElementsByClassName('bg-img')[0];
+        if (window.innerWidth <= 1100) {
+            console.log(window.innerWidth);
+            bgImage.src = "../../../assets/media/bg-logo-dik.png"; 
+        } else {
+            bgImage.src = "../../../assets/media/bg-logo.png";
+        }
+    }
+    window.onload = changeImage;
+    window.onresize = changeImage;
